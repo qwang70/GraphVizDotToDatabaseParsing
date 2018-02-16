0fe50fcf37e7d09eb5486f5eba3ecd41f6373ac5
qwang70 <qiwen.wang@163.com>
2018-02-16 13:01:59 -0600

    draft

--- PowersetObj.py
+++ PowersetObj.py
@@ -1,3 +1,5 @@
+import copy
+
 class TypeHierachy(object):
     def __init__(self,root, propertyList):
         self.root = root
@@ -10,23 +12,45 @@ class TypeHierachy(object):
 
         # one element in set
         self.typeNodes[1] = []
-        self.typeEdges[1] = []
+        # self.typeEdges[1] = []
         for nodeType, nodeVal in root.items():
             self.typeNodes[1].append(
-                    TypeNode({nodeType: nodeVal}, 1, propertyList))
+                TypeNode({nodeType: nodeVal}, 1, propertyList))
+
+        # build multiple level based on the information in the previous level
+        # | {A (a1, a2), B (b1, b2)} | {C (c1, c2), B (b1, b3)} | => {A (a1, a2), B (b1), C (c1, c2)}
         numLevel = len(self.typeNodes[1])
+        # iterate through levels
         for level in range(2, numLevel):
-            for firstSet in range(len(self.typeNodes[level-1])):
-                for secondSet in range(firstlen(self.typeNodes[level-1])):
+            lastLevelNodes = self.typeNodes[level-1]
+            # iterate through the first set
+            for firstSet in range(len(lastLevelNodes)):
+                # iterate through the second set
+                for secondSet in range(firstSet+1, len(lastLevelNodes)):
+                    #TODO: order the typeNodes through defining a comparison function
+                    # get dictionaries
+                    newValToEle = copy.deepcopy(lastLevelNodes[firstSet].typeValsToEleId)
+                    valToEle2 = lastLevelNodes[secondSet].typeValsToEleId
+                    #print(valToEle1)
+
+                    # get key sets union
+                    for key, value in valToEle2.items():
+                        if key not in newValToEle:
+                            newValToEle[key] = value
+                    # TODO: check if key nset no in pairs
+                    print(newValToEle)
+                    TypeNode(newValToEle, level, propertyList)
+
+
             # check combinatin of val of every pair of nodes
         for i in range(len(self.typeNodes[1])):
             print(self.typeNodes[1][i].typeValsToEleId)
             
-            
-
-
 class TypeNode(object):
     """
+        Record the shared elements by different node types.
+        typeSet: a set of types
+        typeValsToEleId: value to element dictionary. value is a tuple
     {
     a: {aa:[1,2], 
            ab[3]}
@@ -51,6 +75,7 @@ class TypeNode(object):
         self.outEdges = {}
 
         self.level = level
+
     def constructTypeValsToEleId(self, mapping):
         isSetAllEle = False
         if mapping is not None:
@@ -72,7 +97,6 @@ class TypeNode(object):
             valSet = set()
             for attKey in self.typeSet:
                 valSet = valSet | {self.propertyList[eleId].getAttrVal(attKey)}
-                print(attKey,valSet)
             if tuple(valSet) not in self.typeValsToEleId:
                 self.typeValsToEleId[tuple(valSet)] = {eleId}
             else:
