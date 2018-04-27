ae66138b3dc2f35bfbc56125bcaa0085f65058df
qwang70 <qiwen.wang@163.com>
2018-04-27 11:02:28 -0500

    render all examples

--- clean.sh
+++ clean.sh
@@ -4,6 +4,6 @@ SCRIPT_FOLDER="script"
 for example in ${EXAMPLE_FOLDER}/**; do
     # input and output folders
     outputFolder="${example}/output"
-    echo $outputFolder
     rm -rf $outputFolder
 done
+echo "done clean output folder"
--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 EXAMPLE_FOLDER="example"
 SCRIPT_FOLDER="script"
 for example in ${EXAMPLE_FOLDER}/**; do
+    echo "Begin running the example folder: '${example}'"
     # input and output folders
     inputFolder="${example}/input"
     outputFolder="${example}/output"
     for filename in ${inputFolder}/*.gv; do
-        python3 ${SCRIPT_FOLDER}/graphviz_parser.py $filename -config test.json 
+        python3 ${SCRIPT_FOLDER}/graphviz_parser.py $filename -config config.json 
         gvFileBaseName=$(basename $filename | cut -d"." -f1)
-        echo $gvFileBaseName
         mkdir -p ${outputFolder}
         find ${outputFolder} -name '*.gv' | while read outputGvFile; do
             outputGvFileName=$(echo $outputGvFile | cut -d"." -f1)
